bd0ab349c48b07e93fbfb9d35fd5d920b8254fc4
Bhupendra Dodiya <bdbhupendra123@gmail.com>
2023-03-15 19:22:43 +0530

    create classbased views and perform operation

--- E_comm/app/admin.py
+++ E_comm/app/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Cart
 # Register your models here.
 
 @admin.register((Product))
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title','selling_price','discounted_price','description','brand','category','product_image']
+
+@admin.register((Cart))
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id','user','product','quantity']
--- E_comm/app/models.py
+++ E_comm/app/models.py
@@ -13,8 +13,13 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     selling_price = models.FloatField()
     discounted_price = models.FloatField()
-    description = models.TextField(max_length=100)
+    description = models.TextField()
     brand = models.CharField(max_length=100)
-    category = models.CharField(choices= product_choice,default='mobile',max_length=100)
+    category = models.CharField(choices= product_choice,max_length=100)
     product_image = models.ImageField(upload_to='product/')
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default =1)
     
